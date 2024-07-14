bf3c760ab6c6716c76bdda55a74bb56e81a1c619
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-07-14 16:24:42 +0530

    chat-interface

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,6 +1,29 @@
 import React from "react";
 import "./chat.css";
+import userImg from "../../pictures/profile-2.png";
+import { LuPhone } from "react-icons/lu";
+import { IoVideocamOutline } from "react-icons/io5";
+import { BsInfoCircle } from "react-icons/bs";
 
 export default function Chat() {
-  return <div className="chat">Chat</div>;
+  return (
+    <div className="chat">
+      <div className="top">
+        <div className="user">
+          <img src={userImg} alt="user-img" />
+          <div className="texts">
+            <span>Vaishnavi Patil</span>
+            <p>Lorem ipsum dolor, sit amef</p>
+          </div>
+        </div>
+        <div className="icons">
+          <LuPhone />
+          <IoVideocamOutline />
+          <BsInfoCircle />
+        </div>
+      </div>
+      <div className="center"></div>
+      <div className="bottom"></div>
+    </div>
+  );
 }
--- src/components/list/chatList/chatList.js
+++ src/components/list/chatList/chatList.js
@@ -1,5 +1,64 @@
 import "./chatList.css";
+import pfImage from "../../../pictures/profile-1.png";
 
 export default function ChatList() {
-  return <div className="ChatList">ChatList</div>;
+  return (
+    <div className="ChatList">
+      <div className="item">
+        <img src={pfImage} alt="profile-1" className="pfp" />
+        <div className="texts">
+          <span>Vaishnavi Patil</span>
+          <p>Hello there!</p>
+        </div>
+      </div>
+
+      <div className="item">
+        <img src={pfImage} alt="profile-1" className="pfp" />
+        <div className="texts">
+          <span>Vaishnavi Patil</span>
+          <p>Hello there!</p>
+        </div>
+      </div>
+
+      <div className="item">
+        <img src={pfImage} alt="profile-1" className="pfp" />
+        <div className="texts">
+          <span>Vaishnavi Patil</span>
+          <p>Hello there!</p>
+        </div>
+      </div>
+
+      <div className="item">
+        <img src={pfImage} alt="profile-1" className="pfp" />
+        <div className="texts">
+          <span>Vaishnavi Patil</span>
+          <p>Hello there!</p>
+        </div>
+      </div>
+
+      <div className="item">
+        <img src={pfImage} alt="profile-1" className="pfp" />
+        <div className="texts">
+          <span>Vaishnavi Patil</span>
+          <p>Hello there!</p>
+        </div>
+      </div>
+
+      <div className="item">
+        <img src={pfImage} alt="profile-1" className="pfp" />
+        <div className="texts">
+          <span>Vaishnavi Patil</span>
+          <p>Hello there!</p>
+        </div>
+      </div>
+
+      <div className="item">
+        <img src={pfImage} alt="profile-1" className="pfp" />
+        <div className="texts">
+          <span>Vaishnavi Patil</span>
+          <p>Hello there!</p>
+        </div>
+      </div>
+    </div>
+  );
 }
