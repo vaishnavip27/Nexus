cd72f9c29a9a11af3d4cad4b859c058ac89cebed
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-07-16 01:23:56 +0530

    yay

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -1,17 +1,67 @@
 import React from "react";
 import "./detail.css";
 import profileImg from "../../pictures/profile-1.png";
+import { IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
+import photoImg from "../../pictures/scenery.jpg";
 
 export default function Detail() {
   return (
     <div className="detail">
       <div className="p-user">
         <img src={profileImg} alt="profile-img" />
-        <h2>Vaishnavi Patil</h2>
+        <h3>Vaishnavi Patil</h3>
         <p>@vai_shhh27</p>
       </div>
+
       <div className="info">
-        <div className="title"></div>
+        <div className="option">
+          <div className="title">
+            <span>About</span>
+            <IoIosArrowUp className="up-arrow" />
+          </div>
+        </div>
+      </div>
+
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Medis, Files and Links</span>
+            <IoIosArrowDown className="arrow-down" />
+          </div>
+
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photo-detail">
+                <img src={photoImg} alt="pic" />
+              </div>
+              <div className="photo-detail">
+                <img src={photoImg} alt="pic" />
+              </div>
+              <div className="photo-detail">
+                <img src={photoImg} alt="pic" />
+              </div>
+              <div className="photo-detail">
+                <img src={photoImg} alt="pic" />
+              </div>
+              <div className="photo-detail">
+                <img src={photoImg} alt="pic" />
+              </div>
+              <div className="photo-detail">
+                <img src={photoImg} alt="pic" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Privacy and Support</span>
+            <IoIosArrowUp className="up-arrow" />
+          </div>
+        </div>
       </div>
     </div>
   );
