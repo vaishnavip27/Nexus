16e858a9f12efb638b2ea506faa533c82a480bd0
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-07-15 17:44:08 +0530

    message UI

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./chat.css";
 import userImg from "../../pictures/profile-2.png";
+import sceneImg from "../../pictures/scenery.jpg";
 import { LuPhone } from "react-icons/lu";
 import { IoVideocamOutline } from "react-icons/io5";
 import { BsInfoCircle } from "react-icons/bs";
@@ -41,8 +42,81 @@ export default function Chat() {
           </div>
         </div>
       </div>
-      <div className="center"></div>
 
+      {/* the message section */}
+      <div className="center">
+        <div className="message">
+          <img src={userImg} alt="image" />
+          <div className="texts">
+            <p>
+              Loerem ipsun hey there how are you can i meet you look sweet and
+              chu yes his name is vaishnavi and she is very strong and confifent
+              and cool and hardowkrijh and ahe knows hwta to do in future
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Loerem ipsun hey there how are you can i meet you look sweet and
+              chu yes his name is vaishnavi and she is very strong and confifent
+              and cool and hardowkrijh and ahe knows hwta to do in future
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message">
+          <img src={userImg} alt="image" />
+          <div className="texts">
+            <p>
+              Loerem ipsun hey there how are you can i meet you look sweet and
+              chu yes his name is vaishnavi and she is very strong and confifent
+              and cool and hardowkrijh and ahe knows hwta to do in future
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <p>
+              Loerem ipsun hey there how are you can i meet you look sweet and
+              chu yes his name is vaishnavi and she is very strong and confifent
+              and cool and hardowkrijh and ahe knows hwta to do in future
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message">
+          <img src={userImg} alt="image" />
+          <div className="texts">
+            <p>
+              Loerem ipsun hey there how are you can i meet you look sweet and
+              chu yes his name is vaishnavi and she is very strong and confifent
+              and cool and hardowkrijh and ahe knows hwta to do in future
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+
+        <div className="message own">
+          <div className="texts">
+            <img src={sceneImg} alt="me" />
+            <p>
+              Loerem ipsun hey there how are you can i meet you look sweet and
+              chu yes his name is vaishnavi and she is very strong and confifent
+              and cool and hardowkrijh and ahe knows hwta to do in future
+            </p>
+            <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+
+      {/* message send container */}
       <div className="bottom">
         <div className="input-wrapper">
           <MdEmojiEmotions
