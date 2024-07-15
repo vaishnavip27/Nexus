638d2a9b0b8c0e3be8916007d7efa1ac0b96a3a3
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-07-15 18:01:28 +0530

    yes

--- src/components/detail/Detail.js
+++ src/components/detail/Detail.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./detail.css";
+import profileImg from "../../pictures/profile-1.png";
 
 export default function Detail() {
-  return <div className="detail">Detail</div>;
+  return (
+    <div className="detail">
+      <div className="p-user">
+        <img src={profileImg} alt="profile-img" />
+        <h2>Vaishnavi Patil</h2>
+        <p>@vai_shhh27</p>
+      </div>
+      <div className="info">
+        <div className="title"></div>
+      </div>
+    </div>
+  );
 }
