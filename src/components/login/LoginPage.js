fc02122423f7b7d2bf2e87662fa3e2ff5610e9c7
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-07-18 19:39:21 +0530

    issue

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -3,7 +3,23 @@ import "./loginPage.css";
 export default function LoginPage() {
   return (
     <div className="login-container">
-      <span className="head-container-2">Nexus</span>
+      <div className="head-container">
+        <span>Nexus</span>
+      </div>
+
+      <div className="form-container">
+        <div className="form-group">
+          <label for="email">Email</label>
+          <br />
+          <input type="email" id="email" name="email" />
+        </div>
+
+        <div className="form-container">
+          <label for="password">Password</label>
+          <br />
+          <input type="password" id="password" name="password" />
+        </div>
+      </div>
     </div>
   );
 }
