44b6fcecc9be53b758df26fa6f9042d63a5afe01
Vaishnavi Patil <vaishnavip276@gmail.com>
2024-07-18 13:34:13 +0530

    final

--- src/components/addUser/addUser.js
+++ src/components/addUser/addUser.js
@@ -1,5 +1,15 @@
 import "./addUser.css";
 
 export default function AddUser() {
-  return <div className="AddUser"></div>;
+  return (
+    <div className="AddUser">
+      <form>
+        <input type="text" placeholder="Username" name="username" />
+        <button>Search</button>
+      </form>
+      <div className="user">
+        <div className="detail"></div>
+      </div>
+    </div>
+  );
 }
